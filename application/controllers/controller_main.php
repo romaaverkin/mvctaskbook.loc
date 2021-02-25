b611def9bb9760ae31f224f0654f8d50cc4883e0
Roman <romaaverkin@gmail.com>
2021-02-25 16:58:25 +0700

    Test

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -8,7 +8,7 @@ class Controller_Main extends Controller
         $task_model = $this->require_model('task_model');
         $data['task_list'] = $task_model->get_task_list();
 
-        echo $_SERVER['HTTP_HOST'];
+//        echo $_SERVER['HTTP_HOST'];
         $this->view->generate('main_view.php', 'template_view.php', $data['task_list']);
     }
 }
