ccfd3631ebdfe1112c38dd7d943939ad08710690
RomanAverkin <romaaverkin@gmail.com>
2021-02-28 22:32:06 +0700

    Первый вариант формы

--- application/views/task_view.php
+++ application/views/task_view.php
@@ -1,23 +1,23 @@
-<section>
-    <div class="container-xxl">
+<div class="container-xxl">
+    <div class="row">
+        <div class="panel-heading">Новая задача</div>
         <form action="/task/add/" method="post" enctype="multipart/form-data" id="addTaskForm">
-            <legend>Новая задача</legend>
-            <div class="mb-3">
-                <label for="name">Имя</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Имя" required>
+            <div class="form-group">
+                <label for="name">Логин</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Логин" required>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Электронная почта</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Электронная почта"
-                       aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">Мы никогда никому не передадим вашу электронную почту.</div>
+            <div class="form-group">
+                <label for="email">Электронная почта</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Электронная почта"
+                       required>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Текст задачи</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Текст задачи"
-                          rows="3"></textarea>
+            <div class="form-group">
+                <label for="task">Задача</label>
+                <textarea class="form-control" rows="3" id="task" name="task" placeholder="Текст задачи"
+                          required></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Добавить задачу</button>
+            <hr>
+            <button type="submit" class="btn btn-primary pull-right">Новая задача</button>
         </form>
     </div>
-</section>
+</div>
